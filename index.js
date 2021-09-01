5226524a4437e374d06429fed785c38bba21c22b
Aziz Kurt <azizkurt@Aziz-Mac-mini.local>
2021-09-01 21:37:36 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/apithon.com.tr/privkey.pem', 'utf8')
+const certificate = fs.readFileSync('/etc/letsencrypt/live/apithon.com.tr/fullchain.pem', 'utf8')
+const credentials = {
+   key: privateKey,
+   cert: certificate
+}
+const app = require("express")();
+const server = require("https").createServer(credentials,app);
+const cors = require("cors");
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Running");
+});
+
+io.on("connection", (socket) => {
+  socket.emit("me", socket.id);
+
+  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("callUser", {
+      signal: signalData,
+      from,
+      name,
+    });
+  });
+
+  socket.on("updateMyMedia", ({ type, currentMediaStatus }) => {
+    console.log("updateMyMedia");
+    socket.broadcast.emit("updateUserMedia", { type, currentMediaStatus });
+  });
+
+  socket.on("msgUser", ({ name, to, msg, sender }) => {
+    io.to(to).emit("msgRcv", { name, msg, sender });
+  });
+
+  socket.on("answerCall", (data) => {
+    socket.broadcast.emit("updateUserMedia", {
+      type: data.type,
+      currentMediaStatus: data.myMediaStatus,
+    });
+    io.to(data.to).emit("callAccepted", data);
+  });
+  socket.on("endCall", ({ id }) => {
+    io.to(id).emit("endCall");
+  });
+});
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
